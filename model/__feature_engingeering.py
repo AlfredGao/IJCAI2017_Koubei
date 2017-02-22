e255c8c29c7289afa8d1b46bd9225f062f7d902e
Hey!Alfred <yu.gao.whut@gmail.com>
2017-02-21 16:51:41 -0800

    Random Forest score: 0.08432580

---
+++ model/__feature_engingeering.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# encoding = uft-8
+"""
+@Author: Alfred Gao
+"""
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import PolynomialFeatures
+from __utils import *
+
+
+shop_date_pay = pd.read_csv('../dataset/preprocessed/shop_date_dapay.csv')
+shop_info = pd.read_csv('../dataset/preprocessed/shop_info_0221.csv')
+week_1 = select_days_range('2016-09-20','2016-09-26',shop_date_pay)
+week_2 = select_days_range('2016-10-11','2016-10-17',shop_date_pay)
+week_3 = select_days_range('2016-10-18','2016-10-24',shop_date_pay)
+week_4 = select_days_range('2016-10-25','2016-10-31',shop_date_pay)
+#----------------------------------Data Clean-----------------------------------
+#
+#
+#------------------------------Train data feature-------------------------------
+#Use the week1 week2 week3 as the training set and week4 as y label
+#Create the raw data frame of train_feature_raw
+train_feature_raw = week_1.join(week_2.join(week_3))
+#Find the rows contained the null value in training data feature
+train_null_value = find_null(train_feature_raw)
+#Smooth the null value
+train_feature = smooth_null(train_null_value, train_feature_raw)
+#Handle the Null value manually.
+train_feature.iloc[809] = [7,5,4,6,9,6,11,7,5,4,6,9,6,11,7,5,4,6,9,6,11]
+
+#------------------------------Train data label---------------------------------
+#Use the week2 week3 week4 as the testing data set.
+#Find the rows contained the null value in training data feature
+label_null_value = find_null(week_4)
+#Smooth the null value
+train_label = smooth_null(label_null_value, week_4)
+#Handle the Null value manually.
+train_label.iloc[1706] = train_label.iloc[1706].fillna(train_feature.iloc[1706].mean(), axis=0)
+train_label.iloc[1823] = train_label.iloc[1823].fillna(train_feature.iloc[1823].mean(), axis=0)
+print len(find_null(train_label))
+
+
+#----------------------------Construct the feature------------------------------
+# train_feature.reset_index()
+train_sum = train_feature.sum(axis=1)
+print train_sum
+train_mean = train_feature.mean(axis=1)
+train_std = train_feature.std(axis=1)
+train_max = train_feature.max(axis=1)
+train_min = train_feature.min(axis=1)
+train_median = train_feature.median(axis=1)
+train_mad = train_feature.mad(axis=1)
+train_var = train_feature.var(axis=1)
+week_end = ['2016-09-24','2016-09-25','2016-10-15','2016-10-16','2016-10-22','2016-10-23']
+train_weekend_ration = train_feature[week_end].sum(axis=1)/train_sum
+train_day_pay = shop_info['per_pay']
+train_city_level = shop_info['city_level']
+poly = PolynomialFeatures(2, interaction_only = True, include_bias=False)
+# print poly.fit_transform(train_feature).shape
+
+train_feature = transfer_2dataframe(poly.fit_transform(train_feature))
+train_feature['sum'] = train_sum
+train_feature['mean'] = train_mean
+train_feature['var'] = train_var
+train_feature['weekend'] = train_weekend_ration
+train_feature['day_pay'] = train_day_pay
+train_feature['city_level'] = train_city_level
+# print train_feature
+train_feature.to_csv('../train/train_feature.csv')
+train_label.to_csv('../train/train_label.csv')
+# print train_weekend_ration
+
+
+test_feature_raw = week_2.join(week_3.join(week_4))
+test_null_value = find_null(test_feature_raw)
+test_feature = smooth_null(test_null_value, test_feature_raw)
+
+
+test_sum = test_feature.sum(axis=1)
+print test_sum
+test_mean = test_feature.mean(axis=1)
+test_std = test_feature.std(axis=1)
+test_max = test_feature.max(axis=1)
+test_min = test_feature.min(axis=1)
+test_median = test_feature.median(axis=1)
+test_mad = test_feature.mad(axis=1)
+test_var = test_feature.var(axis=1)
+week_end = ['2016-10-15','2016-10-16','2016-10-22','2016-10-23','2016-10-29','2016-10-30']
+test_weekend_ration = test_feature[week_end].sum(axis=1)/test_sum
+test_day_pay = shop_info['per_pay']
+test_city_level = shop_info['city_level']
+poly = PolynomialFeatures(2, interaction_only = True, include_bias=False)
+# print poly.fit_transform(train_feature).shape
+
+test_feature = transfer_2dataframe(poly.fit_transform(test_feature))
+test_feature['sum'] = test_sum
+test_feature['mean'] = test_mean
+test_feature['var'] = test_var
+test_feature['weekend'] = test_weekend_ration
+test_feature['day_pay'] = test_day_pay
+test_feature['city_level'] = test_city_level
+# print train_feature
+test_feature.to_csv('../train/test_feature.csv')
+# print train_weekend_ration
---
+++ model/__utils.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+# encoding = uft-8
+"""
+@Author: Alfred Gao
+"""
+
+import numpy as np
+import pandas as pd
+import warnings
+
+
+def get_result(result):
+    """Double the one week result to two week.
+    Args:
+        @result: The pandas dataframe of result. Type: pandas.DataFrame
+
+    """
+    if(len(result.shape)==1):
+        df = pd.DataFrame(result,columns=[0])
+    else:
+        df = pd.DataFrame(result,columns=['col_'+str(i) for i in range(result.shape[1])])
+    df.insert(0,'shop_id',[i for i in range(1,2001)])
+    df = pd.merge(df,df,on='shop_id')
+    return df
+
+
+def select_days_range(start_date, end_date, data_pandas):
+    """Select days range between two dates.
+
+
+    Args:
+        @start_date: The start date of select. Type: datetime
+        @end_date: The end date of select.  Type: datetime
+        @data_pandas: The data's dataframe.  Type: pandas.DataFrame
+
+
+    Returns:
+        @days_range: A dataframe to coressponding data fetched.
+    """
+    days_range = data_pandas.loc[:,start_date:end_date]
+    return days_range
+
+
+def find_null(data_pandas):
+    """Find the rows contains null value in data frame.
+
+
+    Args:
+        @data_pandas: The data's dataframe. Type: DataFrame
+
+
+    Return:
+        @null_rows: The list contained the index of rows contained null value.
+    """
+    null_rows = pd.isnull(data_pandas).any(1).nonzero()[0]
+    return null_rows
+
+
+def smooth_null(null_rows, data_pandas_need_smooth):
+    """Smooth the null value in data frame. In this function the null value is \
+        filled by the mean of row which the null value located.
+
+
+    Args:
+        @null_rows: The list contained the index of rows contained null value. Type: List
+        @data_pandas_need_smooth: The data frame need to smooth the null value. Type: pandas.Dataframe
+
+
+    Return:
+        @data_pandas_need_smooth: The null has been smoothed.
+
+
+    Raise:
+        Error: The id list of shop_id whose null value cannot be smoothed. It will happen when\
+        all value are null and cannot get mean.
+    """
+    for null_index in null_rows:
+        data_pandas_need_smooth.iloc[null_index] = data_pandas_need_smooth \
+            .iloc[null_index].fillna(data_pandas_need_smooth.iloc[null_index].mean(),axis=0)
+    null_recheck = find_null(data_pandas_need_smooth)
+    if len(null_recheck) != 0:
+        warnings.warn("There exist the value cannot be smoothed")
+        print ("The shop id is/are: {}").format(null_recheck)
+    return data_pandas_need_smooth
+
+
+def transfer_2dataframe(numpy_arr, col_name='col_'):
+    """Transfer Numpy array to dataframe
+
+
+    Args:
+        @numpy_arr: The numpy array that need to be transfered. Type: Numpy.array
+        @col_name: The col_name. The default value is 'col_'
+
+
+    Return:
+        @df: The dataframe that tranfer to.
+    """
+    if(len(numpy_arr.shape)==1):
+        df = pd.DataFrame(numpy_arr,columns=['col_0'])
+    else:
+        df = pd.DataFrame(numpy_arr,columns=[col_name+str(i) for i in range(numpy_arr.shape[1])])
+    return df
+
+
+def cal_offline_score(pre_y, real_y):
+    """Calculte the offline score according to the score function on Tianchi.
+
+
+    Args:
+        @pre_y: The prediction value
+        @real_y: The true value
+
+
+    Return:
+        @L: The offline score of the prediction.
+
+
+    Raise:
+        Error: When the shapes of prediction and real are different.
+    """
+    score_sum = 0.
+    assert pre_y.shape == real_y.shape
+    for shop_index in range(pre_y.shape[0]):
+        for day in range(pre_y.shape[1]):
+            score = (pre_y[shop_index][day]-real_y[shop_index][day])/\
+            (pre_y[shop_index][day]+real_y[shop_index][day])
+            score = abs(score)
+            score_sum = score_sum + score
+    L = score_sum/(pre_y.shape[0]*pre_y.shape[1])
+    return L
